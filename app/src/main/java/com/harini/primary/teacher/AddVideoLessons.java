447eaecec4e242700f1208a3f9b91721cc772a5f
buminduyasith <bumiduyasith@gmail.com>
2021-03-19 14:20:56 +0530

    add video lessons created. class still hardcoded

--- app/src/main/java/com/harini/primary/teacher/AddVideoLessons.java
+++ app/src/main/java/com/harini/primary/teacher/AddVideoLessons.java
@@ -16,15 +16,18 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -37,6 +40,7 @@ import com.harini.primary.R;
 import com.harini.primary.adapters.AddVideoLessonsAdapter;
 import com.harini.primary.adapters.AnnouncementRecycleViewAdapter;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -59,6 +63,9 @@ public class AddVideoLessons extends AppCompatActivity {
     private AddVideoLessonsAdapter adapter;
 
     private LinearLayout videolessons_noresults_container;
+
+    private SweetAlertDialog pDialog;
+
 
 
     private static final String TAG = "videolessondbug";
@@ -84,7 +91,7 @@ public class AddVideoLessons extends AppCompatActivity {
         Fab_addvideo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //openDialog();
+                openDialog();
             }
         });
 
@@ -174,6 +181,97 @@ public class AddVideoLessons extends AppCompatActivity {
         });
 
 
+
+
+
+
+
+
+    }
+
+
+    private void openDialog(){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(AddVideoLessons.this);
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.layout_dialog_add_videolessons,null);
+
+        final TextInputLayout title = view.findViewById(R.id.TIL_video_title);
+        final TextInputLayout urlcode = view.findViewById(R.id.TIL_url_code);
+
+
+
+        builder.setView(view)
+                .setTitle("Video Lessons")
+                .setPositiveButton("done", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+
+                        final DialogInterface d =  dialog;
+
+                        Log.d(TAG, "onClick: postive click");
+                        String yturlcode =urlcode.getEditText().getText().toString();
+                        String yttitle =title.getEditText().getText().toString();
+
+                        if(yttitle.isEmpty() || yturlcode.isEmpty()){
+                            Log.d(TAG, "onClick: empty");
+                            Toast.makeText(AddVideoLessons.this,"required_field_missing",Toast.LENGTH_SHORT);
+                            return;
+                        }
+
+                        Timestamp timestamp = new Timestamp(new Date());
+                        Map<String, Object> video = new HashMap<>();
+                        video.put("title",yttitle);
+                        video.put("videoid",yturlcode);
+                        video.put("timestamp",timestamp);
+                        video.put("class","5A");
+
+                        db.collection("VideoLessons").add(video)
+                                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                    @Override
+                                    public void onSuccess(DocumentReference documentReference) {
+                                        Log.d(TAG, "onSuccess: ");
+                                        pDialog = new SweetAlertDialog(AddVideoLessons.this, SweetAlertDialog.SUCCESS_TYPE);
+                                        pDialog.setCancelable(false);
+                                        pDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+                                        pDialog.setTitleText("Video Lessons");
+                                        pDialog.setContentText(" Video Lesson added successfully..");
+                                        pDialog.setConfirmButton("Done\n", new SweetAlertDialog.OnSweetClickListener() {
+                                            @Override
+                                            public void onClick(SweetAlertDialog sweetAlertDialog) {
+                                                pDialog.dismissWithAnimation();
+
+                                            }
+                                        });
+                                        d.dismiss();
+                                        pDialog.show();
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+
+                                Log.d(TAG, "onFailure: ");
+                                pDialog = new SweetAlertDialog(AddVideoLessons.this, SweetAlertDialog.ERROR_TYPE);
+                                pDialog.setCancelable(true);
+                                pDialog.setTitleText("Video Lessons");
+                                pDialog.setContentText(e.getLocalizedMessage());
+                                d.dismiss();
+                                pDialog.show();
+                                Log.d(TAG, "onFailure: "+e.getMessage());
+                            }
+                        });
+
+
+                    }
+                })
+
+                .setNegativeButton("Cancel", null);
+
+        final AlertDialog dialog = builder.create();
+
+        dialog.show();
+       // dialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
 
 
 
