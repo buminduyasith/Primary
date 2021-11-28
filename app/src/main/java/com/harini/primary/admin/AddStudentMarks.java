6e9a023d8d093806c80a92b8bb6690b11f58cbea
hariniamarasinghe <chaudari.amarasinghe@gmail.com>
2021-11-28 13:05:50 +0530

    title text change

--- app/src/main/java/com/harini/primary/admin/AddStudentMarks.java
+++ app/src/main/java/com/harini/primary/admin/AddStudentMarks.java
@@ -687,7 +687,7 @@ public class AddStudentMarks extends AppCompatActivity {
         pDialog = new SweetAlertDialog(AddStudentMarks.this, SweetAlertDialog.PROGRESS_TYPE);
         pDialog.setCancelable(false);
         pDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
-        pDialog.setTitleText("please wait student list creating ...");
+        pDialog.setTitleText("please wait  ...");
         //pDialog.setContentText("All marks added successfully..");
         pDialog.show();
         if(grade==null || grade.isEmpty()){
